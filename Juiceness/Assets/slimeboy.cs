0186af90b3f8b1c3f12b6dcfaa316c4a051c2459
Erick Hasse <erickhasse99@gmail.com>
2022-09-06 21:33:49 -0300

    apresentacao 1a semana

--- Juiceness/Assets/slimeboy.cs
+++ Juiceness/Assets/slimeboy.cs
@@ -7,52 +7,39 @@ public class slimeboy : MonoBehaviour
 
     private Animator animator;
 
-    [SerializeField] private GameObject slime_fill;
-    [SerializeField] private GameObject slime_eyes;
-    [SerializeField] private GameObject slime_skin;
-    [SerializeField] private GameObject slime_outline;
+    [SerializeField] private ParticleSystem slimeParticles;
+    [SerializeField] private ParticleSystem marretaParticles;
 
-    private Material slimeOriginal_fill;
-    private Material slimeOriginal_eyes;
-    private Material slimeOriginal_skin;
-    private Material slimeOriginal_outline;
-
-    [SerializeField] private Material slimeAlternative_fill;
-    [SerializeField] private Material slimeAlternative_eyes;
-    [SerializeField] private Material slimeAlternative_skin;
-    [SerializeField] private Material slimeAlternative_outline;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-
-        slimeOriginal_fill = slime_fill.GetComponent<MeshRenderer>().materials[0];
-        slimeOriginal_eyes = slime_eyes.GetComponent<MeshRenderer>().materials[0];
-        slimeOriginal_skin = slime_skin.GetComponent<MeshRenderer>().materials[0];
-        slimeOriginal_outline = slime_outline.GetComponent<MeshRenderer>().materials[0];
     }
 
     public void hitHammer()
     {
         animator.CrossFade("slime_ybot_casting 1", 0.2f);
+        StartCoroutine(hammerAndSlimeParticles());
     }
 
     public void hoverEnterSlime()
     {
-        slime_fill.GetComponent<MeshRenderer>().materials[0] = slimeAlternative_fill;
-        slime_eyes.GetComponent<MeshRenderer>().materials[0] = slimeAlternative_eyes;
-        slime_skin.GetComponent<MeshRenderer>().materials[0] = slimeAlternative_skin;
-        slime_outline.GetComponent<MeshRenderer>().materials[0] = slimeAlternative_outline;
-        Debug.Log("hover Enter slime");
+
     }
 
     public void hoverExitSlime()
     {
-        slime_fill.GetComponent<MeshRenderer>().materials[0] =      slimeOriginal_fill;
-        slime_eyes.GetComponent<MeshRenderer>().materials[0] =      slimeOriginal_eyes;
-        slime_skin.GetComponent<MeshRenderer>().materials[0] =      slimeOriginal_skin;
-        slime_outline.GetComponent<MeshRenderer>().materials[0] =   slimeOriginal_outline;
-        Debug.Log("hover Exit slime");
+
+    }
+
+    IEnumerator hammerAndSlimeParticles()
+    {
+        yield return new WaitForSeconds(0.20f);
+        marretaParticles.Play();
+
+        yield return new WaitForSeconds(2.11f);
+        slimeParticles.Play();
+
     }
 
 }
